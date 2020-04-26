e5fe63e4cc2edf5c63d39a925c6d2670c86be216
Nityananda Gohain <nityanandagohain@gmail.com>
2020-04-26 12:20:46 +0530

    initial setup

--- src_back/views/Home.vue
+++ src_back/views/Home.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button @click="logout">Logout</button>
   </div>
 </template>
 
 <script>
-import firebase from 'firebase';
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'home',
+  name: 'Home',
   components: {
     HelloWorld
-  },
-  methods: {
-    logout: function() {
-      firebase.auth().signOut().then(() => {
-        this.$router.replace('login')
-      })
-    }
   }
 }
 </script>
